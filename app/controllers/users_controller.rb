292c06ecc29bc59dc40c67177a123a7000ab799c
vagrant <vagrant@localhost.localdomain>
2018-01-08 08:48:34 +0000

    add userinfo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,16 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(current_user[:id])
   end
+
+  def update
+    @user = User.find(current_user[:id])
+    if @user.update(user_params)
+      redirect_to users_edit_path
+    else
+      render :edit
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/show'
-
-  get 'users/edit'
-
-  devise_for :users
-  get 'home/index'
-
   root to: "home#index"
+  
+  devise_for :users
 end
