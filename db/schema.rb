e4ab1c7ade04a58bd290cc598d58f3b016fd7512
vagrant <vagrant@localhost.localdomain>
2018-01-21 06:36:06 +0000

    add user status rolle

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180120132349) do
+ActiveRecord::Schema.define(version: 20180121061856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20180120132349) do
     t.date "birthday"
     t.integer "sex", default: 0
     t.string "school_name"
+    t.integer "role", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
