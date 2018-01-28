768b2c09abda6f50305cc16e189bb59e7e503d90
Th0rt <yamaxanado@gmail.com>
2018-01-28 05:51:09 +0000

    [update]add redirecting from home depend on user status

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,11 @@ class HomeController < ApplicationController
   before_action :set_currentuser
 
   def index
-    if user_signed_in? && @user.role == "リクルーター"
-        redirect_to users_path
-    end
-    if user_signed_in? && @user.role == "ゲスト"
-        redirect_to interviews_path( params: { id: @user.id } )
+    if user_signed_in?
+      redirect_to users_path if @user.role == "リクルーター"
+      redirect_to interviews_path( params: { id: @user.id } ) if  @user.role == "ゲスト"
+    else
+      redirect_to new_user_session_path
     end
   end
-  
 end
